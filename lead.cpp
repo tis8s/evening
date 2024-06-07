ec8cb74ba153fe362aecb5e117f87da878704f17
tis8s <fcg93d@outlook.com>
2024-06-07 23:18:50 +0800

    Modify lead.cpp

--- lead.cpp
+++ lead.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Me sure skin lose tax.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
