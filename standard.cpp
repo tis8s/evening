54e51cfcfd4440274317a90725cf03194cb79365
tis8s <fcg93d@outlook.com>
2024-06-27 23:04:07 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# His area miss bill player reality.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
