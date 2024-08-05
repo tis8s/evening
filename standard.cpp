f045a0979d408a1fbd4054b8f696bc10d49a11c9
tis8s <fcg93d@outlook.com>
2024-08-05 23:02:07 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Officer size difficult what method change since help.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
