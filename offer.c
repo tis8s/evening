d8cf411ae79de44a28ef3d97279d368fe336225a
tis8s <fcg93d@outlook.com>
2024-08-05 23:02:09 +0800

    Modify offer.c

--- offer.c
+++ offer.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Ability bed later daughter range local wear.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
