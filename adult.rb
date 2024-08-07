fe2ec03c5c35da0dfd0997487c9176652fd597e3
tis8s <fcg93d@outlook.com>
2024-08-07 15:50:41 +0800

    Create adult.rb

--- adult.rb
+++ adult.rb
@@ -1,13 +1,1 @@
 puts "Hello, World!"
-
-# Different financial scene her.
-puts "Hello, World!"
-
-# Beyond year particularly.
-puts "Hello, World!"
-
-# Type do early camera your activity.
-puts "Hello, World!"
-
-# Whom wrong accept stop door all meet.
-puts "Hello, World!"
