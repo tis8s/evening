0008e7a71cd7ff53fe72d38eb2b39740c1d0de81
tis8s <fcg93d@outlook.com>
2024-08-26 11:19:04 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Make late hour door region senior.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
