6f7af5b770b40580319b208aca7908060588b37c
tis8s <fcg93d@outlook.com>
2024-09-07 13:10:21 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Understand PM part without.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
