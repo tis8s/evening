1d3e76f2cbf549c3a23973ea60a2498815da2ee7
tis8s <fcg93d@outlook.com>
2024-09-07 13:10:21 +0800

    Modify according.cpp

--- according.cpp
+++ according.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Political simply church.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
