9ffdcc4408129e4374592eaae36fd333460538bc
tis8s <fcg93d@outlook.com>
2024-10-09 11:01:17 +0800

    Modify far.cpp

--- far.cpp
+++ far.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Treatment network writer think.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
