2a1410ada5d63e1e729432036ed6093384f4f603
tis8s <fcg93d@outlook.com>
2024-10-12 10:10:06 +0800

    Modify number.c

--- number.c
+++ number.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Natural last protect film amount level cause.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
