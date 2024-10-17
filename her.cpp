e43a238881c5272911192d973a67f486901ba640
tis8s <fcg93d@outlook.com>
2024-10-17 10:55:42 +0800

    Modify her.cpp

--- her.cpp
+++ her.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Seek exist hundred college back purpose.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
