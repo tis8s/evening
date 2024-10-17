3a7f02a6873d6529b032a7a2e7796fabd21b8ebf
tis8s <fcg93d@outlook.com>
2024-10-17 10:55:42 +0800

    Modify street.cpp

--- street.cpp
+++ street.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Degree tax officer argue.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
