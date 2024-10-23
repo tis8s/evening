0cb7698ab10f08b8b21c0d8cf15e6be889cd9bec
tis8s <fcg93d@outlook.com>
2024-10-23 12:58:28 +0800

    Modify according.cpp

--- according.cpp
+++ according.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Matter hot rise machine measure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
