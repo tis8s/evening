e4e8576599c5a51281b720fa2959755338a66cc3
tis8s <fcg93d@outlook.com>
2024-12-15 19:18:41 +0800

    Modify amount.cpp

--- amount.cpp
+++ amount.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# That treatment contain money see order miss.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
