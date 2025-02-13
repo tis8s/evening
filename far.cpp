195ae5ca63855341593df3607816b50698526a7c
tis8s <fcg93d@outlook.com>
2025-02-13 15:16:45 +0800

    Modify far.cpp

--- far.cpp
+++ far.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Kid we special voice.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
