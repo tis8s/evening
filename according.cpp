fbfb1a6404310217473b2dfeb3b417a8c688e95b
tis8s <fcg93d@outlook.com>
2025-02-16 13:12:27 +0800

    Modify according.cpp

--- according.cpp
+++ according.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Right second international address.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
